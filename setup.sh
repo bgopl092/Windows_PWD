405d35447781ba0f28f90359bcbd74aeaa5d6cea
bgopl092 <125370777+bgopl092@users.noreply.github.com>
2023-08-14 13:07:04 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,25 +7,10 @@ apt update -y
 apt upgrade -y
 apt install qemu qemu-utils ovmf -y
 apt install qemu-kvm -y
-wget 'https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz'
+docker run -it -e NGROK_AUTHTOKEN=CRP ngrok/ngrok tcp 3389 > /dev/null 2>&1
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1CClaOwHCfatYDbgYmX0r_TmDxlQOq7il' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1CClaOwHCfatYDbgYmX0r_TmDxlQOq7il" -O windows.tar.xz && rm -rf /tmp/cookies.txt
 tar xvzf windows.tar.xz -C /root
-tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
-rm -rfv windows.tar.xz ngrok-v3-stable-linux-amd64.tgz
-ngrok config add-authtoken $CRP
-echo "Repo: https://github.com/bgopl092/Windows_PWD"
-echo "================================================"
-echo "Choose ngrok region (for better connection)."
-echo "================================================"
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-read -p "Choose ngrok region: " CNR
-ngrok tcp --region $CNR 3389 &>/dev/null &
+rm -rfv windows.tar.xz
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 echo IP Address:
