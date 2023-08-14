87ea507bc95c17865cadcfc83f9027102ce73ffd
bgopl092 <125370777+bgopl092@users.noreply.github.com>
2023-08-14 12:48:27 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,8 +24,8 @@ echo "au - Australia (Sydney)"
 echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
-read -p "Choose ngrok region: " CRP
-ngrok tcp --region $CRP 3389 &>/dev/null &
+read -p "Choose ngrok region: " CNR
+ngrok tcp --region $CNR 3389 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 echo IP Address:
